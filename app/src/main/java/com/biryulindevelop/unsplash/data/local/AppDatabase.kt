28aec13dfd9381cb8e6adac8e0db36be47083376
Biryulin Dmitry <filin2hat@yandex.ru>
2023-06-22 09:51:14 +0300

    Обновляется версия Gradle на 8.0.2, добавляется поддержка Java 17, RecyclerView 1.3.0, view binding delegate, а также обновляются зависимости для androidx.appcompat:appcompat, androidx.lifecycle:lifecycle-livedata-ktx, androidx.lifecycle:lifecycle-viewmodel-ktx, androidx.fragment:fragment-ktx, androidx.navigation:navigation-fragment-ktx, androidx.navigation:navigation-ui-ktx, com.google.android.material:material, и com.github.bumptech.glide:glide.

--- app/src/main/java/com/biryulindevelop/unsplash/data/local/AppDatabase.kt
+++ app/src/main/java/com/biryulindevelop/unsplash/data/local/AppDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.biryulindevelop.unsplash.data.local.entity.PhotoEntity
 
-@Database(entities = [PhotoEntity::class], version = 1)
+@Database(entities = [PhotoEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun getPhotosDao(): PhotosDao
 }
