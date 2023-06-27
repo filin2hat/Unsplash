eb858c119781fda0fe266096064d65fda83cb16b
Dmitriy Biryulin <biryulin.develop@gmail.com>
2023-06-27 19:42:32 +0300

    удалил все что связано с загрузкой изображения

--- app/src/main/java/com/biryulindevelop/unsplash/presentation/screens/photos/details/OnePhotoDetailsFragment.kt
+++ app/src/main/java/com/biryulindevelop/unsplash/presentation/screens/photos/details/OnePhotoDetailsFragment.kt
@@ -2,25 +2,18 @@ package com.biryulindevelop.unsplash.presentation.screens.photos.details
 
 import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.annotation.SuppressLint
-import android.app.DownloadManager
 import android.content.BroadcastReceiver
 import android.content.ContentValues.TAG
-import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
-import android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION
-import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.RECEIVER_EXPORTED
-import androidx.core.content.ContextCompat.registerReceiver
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -44,13 +37,10 @@ class OnePhotoDetailsFragment : Fragment(R.layout.fragment_one_photo_details) {
     private val binding by viewBinding(FragmentOnePhotoDetailsBinding::bind)
     private val viewModel: OnePhotoDetailsViewModel by viewModels()
     private val args by navArgs<OnePhotoDetailsFragmentArgs>()
-    private val downloadManager by lazy {
-        requireContext().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-    }
+
     private lateinit var receiver: BroadcastReceiver
     private var lat: Double? = null
     private var lon: Double? = null
-    private var enableDownloadFlag = false
 
     private val launcher = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
@@ -61,7 +51,7 @@ class OnePhotoDetailsFragment : Fragment(R.layout.fragment_one_photo_details) {
                 getString(R.string.ok_to_download),
                 Snackbar.LENGTH_LONG
             ).show()
-            enableDownloadFlag = true
+            TODO("Not yet implemented")
         } else {
             missingPermissionAlert()
         }
@@ -75,24 +65,12 @@ class OnePhotoDetailsFragment : Fragment(R.layout.fragment_one_photo_details) {
         stateLike()
     }
 
-    override fun onResume() {
-        super.onResume()
-        val filter = IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE)
-        receiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context?, intent: Intent) {
-                val reference = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
-                if (viewModel.downloadID == reference) {
-                    viewModel.getDMStatus(downloadManager)
-                    if (viewModel.success) {
-                        val uri = downloadManager.getUriForDownloadedFile(viewModel.downloadID)
-                        successfulDownloadMsg(uri)
-                    } else {
-                        failedDownloadMsg()
-                    }
-                }
-            }
-        }
-        registerReceiver(requireContext(), receiver, filter, RECEIVER_EXPORTED)
+    private fun failedDownloadMsg() {
+        TODO("Not yet implemented")
+    }
+
+    private fun successfulDownloadMsg(uri: Uri?) {
+        TODO("Not yet implemented")
     }
 
     private fun getLoadingState() {
@@ -125,12 +103,16 @@ class OnePhotoDetailsFragment : Fragment(R.layout.fragment_one_photo_details) {
                             uploadedLocation(state)
                             toolbar(state.data.id)
                             likeClick(state.data)
-                            downloadOnClick(state.data.urls.raw, downloadManager)
+                            downloadOnClick(state.data.urls.raw)
                         }
                     }
                 }
             }
         }
+    }
+
+    private fun downloadOnClick(raw: String) {
+        TODO("Not yet implemented")
     }
 
     @SuppressLint("StringFormatMatches")
@@ -192,44 +174,6 @@ class OnePhotoDetailsFragment : Fragment(R.layout.fragment_one_photo_details) {
         }
     }
 
-    private fun downloadOnClick(url: String, downloadManager: DownloadManager) {
-        binding.downloadButton.setOnClickListener {
-            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                viewModel.startDownLoad(url, downloadManager)
-            } else {
-                checkPermission()
-                if (enableDownloadFlag) {
-                    viewModel.startDownLoad(url, downloadManager)
-                }
-            }
-        }
-    }
-
-    private fun successfulDownloadMsg(uri: Uri) {
-        val mySnackbar = Snackbar.make(
-            binding.myCoordinatorLayout,
-            getString(R.string.download_finished),
-            Snackbar.LENGTH_INDEFINITE
-        )
-        mySnackbar.setAction(R.string.open) {
-            val openIntent = Intent().apply {
-                action = Intent.ACTION_VIEW
-                flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                flags = FLAG_GRANT_READ_URI_PERMISSION
-            }
-            openIntent.setDataAndType(uri, "image/jpg")
-            startActivity(openIntent)
-        }
-        mySnackbar.show()
-    }
-
-    private fun failedDownloadMsg() {
-        Snackbar.make(
-            binding.myCoordinatorLayout,
-            getString(R.string.failed_download),
-            Snackbar.LENGTH_LONG
-        ).show()
-    }
 
     private fun likeClick(photo: PhotoDetails) {
         binding.isLikedButtonView.setOnClickListener {
@@ -284,7 +228,7 @@ class OnePhotoDetailsFragment : Fragment(R.layout.fragment_one_photo_details) {
                 WRITE_EXTERNAL_STORAGE
             ) == PackageManager.PERMISSION_GRANTED
         ) {
-            enableDownloadFlag = true
+            TODO("Not yet implemented")
         } else {
             launcher.launch(arrayOf(WRITE_EXTERNAL_STORAGE))
         }
--- app/src/main/java/com/biryulindevelop/unsplash/presentation/screens/photos/details/OnePhotoDetailsViewModel.kt
+++ app/src/main/java/com/biryulindevelop/unsplash/presentation/screens/photos/details/OnePhotoDetailsViewModel.kt
@@ -1,10 +1,5 @@
 package com.biryulindevelop.unsplash.presentation.screens.photos.details
 
-import android.annotation.SuppressLint
-import android.app.DownloadManager
-import android.database.Cursor
-import android.net.Uri
-import android.os.Environment
 import androidx.lifecycle.viewModelScope
 import com.biryulindevelop.unsplash.data.state.LoadState
 import com.biryulindevelop.unsplash.domain.model.PhotoDetails
@@ -16,7 +11,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
@@ -27,9 +21,6 @@ class OnePhotoDetailsViewModel @Inject constructor(
 
     private val _state = MutableStateFlow<OnePhotoDetailsState>(OnePhotoDetailsState.NotStarted)
     val state = _state.asStateFlow()
-
-    var downloadID = 0L
-    var downloading = true
     var success = false
 
     fun loadPhotoDetails(id: String) {
@@ -45,46 +36,6 @@ class OnePhotoDetailsViewModel @Inject constructor(
             stateLoad.value = LoadState.SUCCESS
             _state.value =
                 OnePhotoDetailsState.Success(onePhotoDetailsUseCase.execute(id = item.id))
-        }
-    }
-
-    fun startDownLoad(url: String, downloadManager: DownloadManager) {
-        val downloadRequest = DownloadManager.Request(Uri.parse(url))
-            .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI)
-            .setTitle("Unsplash photo")
-            .setDestinationInExternalPublicDir(
-                Environment.DIRECTORY_DOWNLOADS,
-                File.separator + "img_unsplsh.jpg"
-            )
-            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-
-        downloadID = downloadManager.enqueue(downloadRequest)
-    }
-
-    @SuppressLint("Range")
-    fun getDMStatus(myDownloadManager: DownloadManager) {
-        viewModelScope.launch(Dispatchers.IO + handler) {
-            val request = DownloadManager.Query().setFilterById(downloadID)
-            var cursor: Cursor?
-            request.setFilterByStatus(DownloadManager.STATUS_FAILED or DownloadManager.STATUS_SUCCESSFUL)
-            while (downloading) {
-                cursor = myDownloadManager.query(request)
-                if (cursor.moveToPosition(0) && downloading) {
-                    val status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
-                    if (status == DownloadManager.STATUS_SUCCESSFUL) {
-                        downloading = false
-                        success = true
-                        cursor.close()
-                    }
-                    if (status == DownloadManager.STATUS_FAILED) {
-                        downloading = false
-                        success = false
-                        cursor.close()
-                    }
-                } else {
-                    cursor.close()
-                }
-            }
         }
     }
 }
