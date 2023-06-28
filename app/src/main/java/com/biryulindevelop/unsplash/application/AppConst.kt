c84d8b5cae597e08969bbf49687ba69de00cdaf8
Biryulin Dmitry <filin2hat@yandex.ru>
2023-06-28 15:12:28 +0300

    для работы требуется обновлять API KEY: ACCESS_KEY / SECRET_KEY. Брать их из кабинета разработчика: https://unsplash.com/developers

--- app/src/main/java/com/biryulindevelop/unsplash/application/AppConst.kt
+++ app/src/main/java/com/biryulindevelop/unsplash/application/AppConst.kt
@@ -1,17 +1,9 @@
 package com.biryulindevelop.unsplash.application
 
 const val REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
-const val ACCESS_KEY = "dq7K-hs0otHTImGklsZeID8Y6HBlDd6WdFfHGEmEElk"
-const val SECRET_KEY = "dw8PNk1WjIz4D0gT5oJDH-F0fGpCh-DtcNqHXjW6pOM"
 
-//const val ACCESS_KEY = "0mWlk4H1JqeVUaZ0S136G2DkDB8N6poiRRNDXIdAsoM"
-//const val SECRET_KEY = "6jPzllGr2RxoPQ9daKVVhTFJtQswY50Ley6YxjEKYCw"
-
-//const val ACCESS_KEY = "JldYVfpRuY9czIZ7Ye2hISDxZkVear9kx-CjRPdyZb0"
-//const val SECRET_KEY = "AQRrx_yDpkfjVazKVZIFbp4LlSoi-vzhCMAMy3hFiCM"
-
-//const val ACCESS_KEY = "5bSR9PSHmHMrwe0hm3Lu2pa7uTtx-pG49Kf-l2lKHNQ"
-//const val SECRET_KEY = "cN2oXv9VpzFjJge_X2rEFlYH4qSRunvSzAYSMHoCSBI"
+const val ACCESS_KEY = "zIF0uoAy_pcdB5jZyTIBzh5SdfkFGslhzXAgUM3xxd8"
+const val SECRET_KEY = "3T59pONdkckll9PpY3morKd3rbnjhcTEJh4QBbT8uAM"
 
 const val TOKEN_NAME = "name_token"
 const val TOKEN_KEY = "key_token"
