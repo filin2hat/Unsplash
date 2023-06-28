6bd65609552872a7ce43ae07465d1909d459d4e0
Biryulin Dmitry <filin2hat@yandex.ru>
2023-06-28 11:03:00 +0300

    загрузка фото работает

--- app/src/main/java/com/biryulindevelop/unsplash/presentation/screens/photos/details/OnePhotoDetailsFragment.kt
+++ app/src/main/java/com/biryulindevelop/unsplash/presentation/screens/photos/details/OnePhotoDetailsFragment.kt
@@ -2,13 +2,18 @@ package com.biryulindevelop.unsplash.presentation.screens.photos.details
 
 import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.annotation.SuppressLint
+import android.app.DownloadManager
 import android.content.BroadcastReceiver
 import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
@@ -30,17 +35,23 @@ import com.biryulindevelop.unsplash.presentation.utils.imgLoader
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @AndroidEntryPoint
 class OnePhotoDetailsFragment : Fragment(R.layout.fragment_one_photo_details) {
+    //private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
 
     private val binding by viewBinding(FragmentOnePhotoDetailsBinding::bind)
     private val viewModel: OnePhotoDetailsViewModel by viewModels()
-    private val args by navArgs<OnePhotoDetailsFragmentArgs>()
+    private val navArgs: OnePhotoDetailsFragmentArgs by navArgs()
+
+    private var lattitude: Double? = null
+    private var longitude: Double? = null
 
     private lateinit var receiver: BroadcastReceiver
-    private var lat: Double? = null
-    private var lon: Double? = null
+    private var isReceiverRegistered = false
 
     private val launcher = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
@@ -49,32 +60,74 @@ class OnePhotoDetailsFragment : Fragment(R.layout.fragment_one_photo_details) {
             Snackbar.make(
                 binding.myCoordinatorLayout,
                 getString(R.string.ok_to_download),
-                Snackbar.LENGTH_LONG
+                Snackbar.LENGTH_SHORT
             ).show()
-            TODO("Not yet implemented")
         } else {
             missingPermissionAlert()
         }
     }
 
-    private fun checkPermission() {
+    private fun checkPermission(url: String) {
         if (ContextCompat.checkSelfPermission(
                 requireActivity(),
                 WRITE_EXTERNAL_STORAGE
             ) == PackageManager.PERMISSION_GRANTED
         ) {
-            TODO("Not yet implemented")
+            startDownload(url)
         } else {
             launcher.launch(arrayOf(WRITE_EXTERNAL_STORAGE))
         }
     }
 
+//    private fun checkPermission(url: String) {
+//        when {
+//            ContextCompat.checkSelfPermission(
+//                requireContext(),
+//                WRITE_EXTERNAL_STORAGE
+//            ) == PackageManager.PERMISSION_GRANTED -> {
+//                startDownload(url)
+//            }
+//
+//            shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE) -> {
+//                showPermissionRequestDialog("permission_title", "write_permission_request") {
+//                    requestPermissionLauncher.launch(WRITE_EXTERNAL_STORAGE)
+//                }
+//            }
+//
+//            else -> {
+//                requestPermissionLauncher.launch(WRITE_EXTERNAL_STORAGE)
+//            }
+//        }
+//    }
+//
+//    private fun showPermissionRequestDialog(
+//        title: String,
+//        body: String,
+//        callback: () -> Unit
+//    ) {
+//        AlertDialog.Builder(requireContext()).also {
+//            it.setTitle(title)
+//            it.setMessage(body)
+//            it.setPositiveButton("Ok") { _, _ ->
+//                callback()
+//            }
+//        }.create().show()
+//    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.loadPhotoDetails(args.photoId)
+        viewModel.loadPhotoDetails(navArgs.photoId)
         getLoadingState()
         locationClick()
         stateLike()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (isReceiverRegistered) {
+            requireActivity().unregisterReceiver(receiver)
+            isReceiverRegistered = false
+        }
     }
 
     private fun getLoadingState() {
@@ -107,7 +160,7 @@ class OnePhotoDetailsFragment : Fragment(R.layout.fragment_one_photo_details) {
                             uploadedLocation(state)
                             toolbar(state.data.id)
                             likeClick(state.data)
-                           // downloadOnClick(state.data.urls.raw)
+                            downloadOnClick(state.data.urls.raw)
                         }
                     }
                 }
@@ -115,16 +168,76 @@ class OnePhotoDetailsFragment : Fragment(R.layout.fragment_one_photo_details) {
         }
     }
 
-    private fun downloadOnClick(raw: String) {
-        TODO("Not yet implemented")
+    private fun downloadOnClick(url: String) {
+        binding.downloadButton.setOnClickListener {
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                startDownload(url)
+            } else {
+                checkPermission(url)
+            }
+        }
+    }
+
+    private fun startDownload(url: String) {
+        startingDownloadMsg()
+        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val fileName = "unsl_$timestamp.jpg"
+
+        val request = DownloadManager.Request(Uri.parse(url))
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName)
+
+        val downloadManager =
+            requireContext().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val downloadId = downloadManager.enqueue(request)
+
+        receiver = object : BroadcastReceiver() {
+            @SuppressLint("Range")
+            override fun onReceive(context: Context?, intent: Intent?) {
+                val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+                if (id == downloadId) {
+                    val query = DownloadManager.Query().setFilterById(downloadId)
+                    val cursor = downloadManager.query(query)
+                    if (cursor.moveToFirst()) {
+                        val status =
+                            cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                        if (status == DownloadManager.STATUS_SUCCESSFUL) {
+                            successfulDownloadMsg()
+                        } else {
+                            failedDownloadMsg()
+                        }
+                    }
+                    cursor.close()
+                }
+            }
+        }
+        val filter = IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE)
+        requireContext().registerReceiver(receiver, filter)
+        isReceiverRegistered = true
     }
 
     private fun failedDownloadMsg() {
-        TODO("Not yet implemented")
+        Snackbar.make(
+            binding.myCoordinatorLayout,
+            getString(R.string.failed_download),
+            Snackbar.LENGTH_SHORT
+        ).show()
     }
 
-    private fun successfulDownloadMsg(uri: Uri?) {
-        TODO("Not yet implemented")
+    private fun successfulDownloadMsg() {
+        Snackbar.make(
+            binding.myCoordinatorLayout,
+            getString(R.string.download_finished),
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun startingDownloadMsg() {
+        Snackbar.make(
+            binding.myCoordinatorLayout,
+            getString(R.string.download_started),
+            Snackbar.LENGTH_SHORT
+        ).show()
     }
 
     @SuppressLint("StringFormatMatches")
@@ -157,18 +270,18 @@ class OnePhotoDetailsFragment : Fragment(R.layout.fragment_one_photo_details) {
 
     private fun uploadedLocation(state: OnePhotoDetailsState.Success) {
         if (state.data.location.position.latitude != null && state.data.location.position.longitude != null) {
-            lat = state.data.location.position.latitude
-            lon = state.data.location.position.longitude
+            lattitude = state.data.location.position.latitude
+            longitude = state.data.location.position.longitude
         }
     }
 
     private fun locationClick() {
         binding.locationView.setOnClickListener {
-            Log.d(TAG, "lat $lat\nlon $lon ")
-            if (lat != null && lon != null) {
-                if (lat != 0.0 && lon != 0.0) {
+            Log.d(TAG, "lat $lattitude\nlon $longitude ")
+            if (lattitude != null && longitude != null) {
+                if (lattitude != 0.0 && longitude != 0.0) {
                     Log.d(TAG, "open map")
-                    locationOnMap(Uri.parse("geo: $lat,$lon"))
+                    locationOnMap(Uri.parse("geo: $lattitude,$longitude"))
                 }
             } else {
                 Log.d(TAG, "don't open map")
